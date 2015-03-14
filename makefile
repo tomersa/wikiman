a73083ac28a52a817a76b915e94683c59ca78f94
Tomer Saban <sab30226@gmail.com>
2015-03-14 22:27:21 +0200

    Fixed installation. Now using /usr/bin and /usr/lib/ for installation

--- makefile
+++ makefile
@@ -1,12 +1,27 @@
-
 all: test 
 
-test: install clean
-	python src/main.py Israel | less
+test:
+	wiki Israel | less
 
-install:
+install: clean
 	python -m pip install wikipedia
+	
+	###Comment out the following line if using fedora###
+	apt-get install less
+	
+	###Uncomment the following line if using fedora###
+	#yum install less
+	
+	##Installing script
+	cp src/wikiman.sh /usr/bin/wiki
+	
+	mkdir /usr/lib/wikiman/
+	cp src/main.py /usr/lib/wikiman/
 	
 clean: 
 	mkdir -p bin
 	rm -f bin/*.bin
+	rm -f /etc/profile.d/wikiman.sh
+	rm -f /usr/bin/wiki
+	rm -rf /usr/lib/wikiman/
+	
--- src/main.py
+++ src/main.py
@@ -2,7 +2,11 @@ import sys
 import wikipedia
 
 if __name__ == "__main__":
-    name_arg = sys.argv[1]
+    if len(sys.argv) < 2:
+				print "Usage: wiki [search term]"
+				sys.exit(0)
+    
+    name_arg = " ".join(sys.argv[1:])
     search_term = wikipedia.search(name_arg)[0]  # Using first result as search
     # output = []
 
@@ -14,20 +18,6 @@ if __name__ == "__main__":
     print u"\n"
     print wiki_page.content.encode('utf-8')
     print u"Based on url: {0}".format(wiki_page.url)
-
-    #output_string = u""
-    #for i in output:
-        #output_string = output_string + i
-
-    #output_string = output_string
-
-    #less_pipe = Popen(['less'], stdin=subprocess.PIPE)
-    #less_pipe.stdin.write(output_string)
-    # for i in output:
-				# print i
-    #for i in output:
-				#print i
-
 
 # >>> wiki_page.links[0]
 # # u'1790 United States Census'
